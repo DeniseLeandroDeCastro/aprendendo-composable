eb35925507ff61eef128e8ff0e9331b68c7758ff
Denise Castro <dmaria.natal@gmail.com>
2023-05-08 09:07:55 -0300

    [ANDROID][COMPOSABLE] - Montando um layout de um card com imagem de um produto

--- app/src/main/java/br/com/denisecastro/delivery/MainActivity.kt
+++ app/src/main/java/br/com/denisecastro/delivery/MainActivity.kt
@@ -3,89 +3,67 @@ package br.com.denisecastro.delivery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.denisecastro.delivery.ui.theme.DeliveryTheme
+import br.com.denisecastro.delivery.ui.theme.Purple200
+import br.com.denisecastro.delivery.ui.theme.Purple500
+import br.com.denisecastro.delivery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "My first composable")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ColumnPreview() {
-    Column {
-        Text("Texto 1")
-        Text("Texto 2")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RowPreview() {
-    Row {
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun BoxPreview() {
-    Box {
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun CustomLayoutPreview() {
-    Column {
-        Text(text = "Column 1")
-        Text(text = "Column 2")
-        Row {
-            Text(text = "Row 1")
-            Text(text = "Row 2")
-        }
-        Box {
-            Row {
-                Text(text = "Row dentro de Box 1")
-                Text(text = "Row dentro de Box 2")
-            }
-            Column {
-                Text(text = "Column dentro de Box 1")
-                Text(text = "Column dentro de Box 2")
+            DeliveryTheme {
+                Surface {
+                    ProductItem()
+                }
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun MyFirstComposable() {
-    Text(text = "Meu primeiro texto")
-    Text(text = "Meu segundo texto maior")
-}
-
-@Preview
-@Composable
-fun MyFirstComposablePreview() {
-    DeliveryTheme {
-        Surface {
-            MyFirstComposable()
-        }
+fun ProductItem() {
+    Column(
+        Modifier
+            .width(200.dp)
+            .height(250.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(brush = Brush.horizontalGradient(colors = listOf(
+                    Purple500, Teal200
+                )))
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Imagem do produto",
+            Modifier
+                .offset(y = (-50).dp)
+                .clip(shape = CircleShape)
+                .align(CenterHorizontally)
+        )
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
     }
 }
+
+
